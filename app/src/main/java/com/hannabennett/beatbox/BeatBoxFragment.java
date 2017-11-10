8a04f0ac8bda258430df6b1e007fc00c5dc8c0f6
Hanna Bennett <bennetthanna22@gmail.com>
2017-11-09 17:13:17 -0800

    Chapter 20, page 377 : Rework main layout file and create BeatBoxFragment and fill out BeatBoxActivity

---
+++ app/src/main/java/com/hannabennett/beatbox/BeatBoxFragment.java
@@ -0,0 +1,13 @@
+package com.hannabennett.beatbox;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by HannaBennett on 11/9/17.
+ */
+
+public class BeatBoxFragment extends Fragment {
+    public static BeatBoxFragment newInstance() {
+        return new BeatBoxFragment();
+    }
+}
