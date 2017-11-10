4932c8b778505f26cdbce575818d9890f69b2e0e
Hanna Bennett <bennetthanna22@gmail.com>
2017-11-10 15:31:58 -0800

    Chapter 20, page 382 : Create SoundHolder and SoundAdapter wired up to list_item_sound

--- app/src/main/java/com/hannabennett/beatbox/BeatBoxFragment.java
+++ app/src/main/java/com/hannabennett/beatbox/BeatBoxFragment.java
@@ -4,11 +4,13 @@ import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.hannabennett.beatbox.databinding.FragmentBeatBoxBinding;
+import com.hannabennett.beatbox.databinding.ListItemSoundBinding;
 
 /**
  * Created by HannaBennett on 11/9/17.
@@ -25,7 +27,38 @@ public class BeatBoxFragment extends Fragment {
                 .inflate(inflater, R.layout.fragment_beat_box, container, false);
 
         binding.recyclerView.setLayoutManager(new GridLayoutManager(getActivity(), 3));
+        binding.recyclerView.setAdapter(new SoundAdapter());
 
         return binding.getRoot();
+    }
+
+    private class SoundHolder extends RecyclerView.ViewHolder {
+        private ListItemSoundBinding mBinding;
+
+        private SoundHolder(ListItemSoundBinding binding) {
+            super(binding.getRoot());
+            mBinding = binding;
+        }
+    }
+
+    private class SoundAdapter extends RecyclerView.Adapter<SoundHolder> {
+
+        @Override
+        public SoundHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            LayoutInflater inflater = LayoutInflater.from(getActivity());
+            ListItemSoundBinding binding = DataBindingUtil
+                    .inflate(inflater, R.layout.list_item_sound, parent, false);
+            return new SoundHolder(binding);
+        }
+
+        @Override
+        public void onBindViewHolder(SoundHolder holder, int position) {
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return 0;
+        }
     }
 }
