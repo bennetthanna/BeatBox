0719e1e1fcde28b2d5a004b2a9f5198543a85688
Hanna Bennett <bennetthanna22@gmail.com>
2017-11-12 15:31:52 -0800

    Chapter 20, page 391 : Create view model with Sound and BeatBox properties

---
+++ app/src/main/java/com/hannabennett/beatbox/SoundViewModel.java
@@ -0,0 +1,26 @@
+package com.hannabennett.beatbox;
+
+/**
+ * Created by HannaBennett on 11/12/17.
+ */
+
+public class SoundViewModel {
+    private Sound mSound;
+    private BeatBox mBeatBox;
+
+    public SoundViewModel(BeatBox beatBox) {
+        mBeatBox = beatBox;
+    }
+
+    public String getTitle() {
+        return mSound.getName();
+    }
+
+    public Sound getSound() {
+        return mSound;
+    }
+
+    public void setSound(Sound sound) {
+        mSound = sound;
+    }
+}
