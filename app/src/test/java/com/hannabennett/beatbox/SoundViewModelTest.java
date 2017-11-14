cec3fa1090b4d7fcbba194a0f1d1b43ac782fe3e
Hanna Bennett <bennetthanna22@gmail.com>
2017-11-14 11:09:24 -0800

    Chapter 21, page 409 : Write failing test that expects the play method to be called on mBeatBox with mSound as a parameter when the btton is clicked

--- app/src/test/java/com/hannabennett/beatbox/SoundViewModelTest.java
+++ app/src/test/java/com/hannabennett/beatbox/SoundViewModelTest.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 
 /**
  * Created by HannaBennett on 11/14/17.
@@ -26,6 +27,13 @@ public class SoundViewModelTest {
     @Test
     public void exposesSoundNameAsTitle() {
         assertThat(mSubject.getTitle(), is(mSound.getName()));
+    }
+
+    @Test
+    public void callsBeatBoxPlayOnButtonClicked() {
+        mSubject.onButtonClicked();
+
+        verify(mBeatBox).play(mSound);
     }
 
 }
