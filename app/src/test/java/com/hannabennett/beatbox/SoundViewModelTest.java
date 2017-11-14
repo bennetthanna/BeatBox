52e82da859557cb2e9ea76dd35de81231f84e3c7
Hanna Bennett <bennetthanna22@gmail.com>
2017-11-14 10:47:28 -0800

    Chapter 21 : page 405 : Create test class CMND + SHFT + T -> Create New Test -> JUnit4 and setUp/@Before -> test folder

---
+++ app/src/test/java/com/hannabennett/beatbox/SoundViewModelTest.java
@@ -0,0 +1,16 @@
+package com.hannabennett.beatbox;
+
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by HannaBennett on 11/14/17.
+ */
+public class SoundViewModelTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+}
