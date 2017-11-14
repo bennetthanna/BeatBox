5a4671f15d007e31541cf3bffd3642a95a67bf0a
Hanna Bennett <bennetthanna22@gmail.com>
2017-11-14 10:52:22 -0800

    Chapter 21, page 406 : Implement test set up with mock BeatBox and SoundViewModel test subject

--- app/src/test/java/com/hannabennett/beatbox/SoundViewModelTest.java
+++ app/src/test/java/com/hannabennett/beatbox/SoundViewModelTest.java
@@ -3,14 +3,22 @@ package com.hannabennett.beatbox;
 import org.junit.Before;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
 
 /**
  * Created by HannaBennett on 11/14/17.
  */
 public class SoundViewModelTest {
+    private BeatBox mBeatBox;
+    private Sound mSound;
+    private SoundViewModel mSubject;
+
     @Before
     public void setUp() throws Exception {
-
+        mBeatBox = mock(BeatBox.class);
+        mSound = new Sound("assetPath");
+        mSubject = new SoundViewModel(mBeatBox);
+        mSubject.setSound(mSound);
     }
 
 }
